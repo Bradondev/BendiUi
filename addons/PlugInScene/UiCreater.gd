132d91a500a4ce359250146a0bf5557388102986
Brandon Martin Del Campo <brandondevprog@gmail.com>
2024-04-01 11:28:12 -0700

    tabs for plugin

--- addons/PlugInScene/UiCreater.gd
+++ addons/PlugInScene/UiCreater.gd
@@ -4,7 +4,7 @@ extends Control
 
 @export var LabelforButton :LineEdit
 @export var FileExplorer : FileDialog
-
+var ButtonScript = load("res://addons/bendiuiplugin/Buttonfunctionality.gd")
 var pathToSaveTo
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,20 +15,22 @@ func _ready():
 func _process(delta):
 	pass
 
-#When the create button is clicked it creates a new pack scene to save
+# When the create button is clicked it creates a new pack scene to save
 func _on_Createbutton_pressed():
 	print_debug("made new button")
 	var packed_scene = PackedScene.new()
 	packed_scene.pack(CreateButton())
-	ResourceSaver.save(packed_scene, pathToSaveTo+"Button.tscn")
+	ResourceSaver.save(packed_scene, pathToSaveTo+packed_scene._bundled["names"][0]+".tscn")
 	
 	
 func CreateButton():
 	var NewButton = Button.new()
+	NewButton.name = LabelforButton.text + "Button"
+	NewButton.set_script(ButtonScript)
 	NewButton.text = LabelforButton.text
 	return NewButton
 
-#Sets the path of the saved packed scene
+# Sets the path of the saved packed scene
 func _on_file_dialog_dir_selected(dir):
 	pathToSaveTo = dir+"/"
 
